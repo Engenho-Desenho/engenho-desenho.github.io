6f9b06444033b2255928425b2e7b56b979b9be81
Colin Brayton <gringolalia@gmail.com>
2020-08-26 12:11:25 -0300

    deploy

---
+++ precache-manifest.000c4b5666adf8c674548fa0bdd50330.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e928b5819043e28529c2",
+    "url": "/css/about.61daf7f5.css"
+  },
+  {
+    "revision": "7b0640c6c5e717248e6e",
+    "url": "/css/app.059c463b.css"
+  },
+  {
+    "revision": "616737a6d7c28f818921",
+    "url": "/css/chunk-vendors.3a6c5b3b.css"
+  },
+  {
+    "revision": "0b72a470344af98b3a5df09dc467daa3",
+    "url": "/img/linotpographer.0b72a470.jpg"
+  },
+  {
+    "revision": "f4b3af62e2e7a8ed525c53fe40b550a8",
+    "url": "/img/morelinot.f4b3af62.jpg"
+  },
+  {
+    "revision": "37b427bdc579e62eba2a06fde4154199",
+    "url": "/img/nyc-nyse.37b427bd.jpg"
+  },
+  {
+    "revision": "dcade5d493c0c508172f0dba96f3716f",
+    "url": "/img/nyc-tenement.dcade5d4.jpg"
+  },
+  {
+    "revision": "aa534f5c3b89adf8dab0396dff929a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e928b5819043e28529c2",
+    "url": "/js/about.589a297a.js"
+  },
+  {
+    "revision": "7b0640c6c5e717248e6e",
+    "url": "/js/app.f93c715f.js"
+  },
+  {
+    "revision": "616737a6d7c28f818921",
+    "url": "/js/chunk-vendors.201a7cc7.js"
+  },
+  {
+    "revision": "cf6d35e92447eebe1f4067517703e68f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
